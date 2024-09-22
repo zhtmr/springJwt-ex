8a721e92da504a4d447707c631f3aeb2d7723fb6
csw <susu199037@gmail.com>
2024-09-22 18:17:04 +0900

    Initial commit

---
+++ src/main/java/com/ex/springjwtex/SpringJwtExApplication.java
@@ -0,0 +1,13 @@
+package com.ex.springjwtex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtExApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJwtExApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ex/springjwtex/SpringJwtExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.springjwtex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtExApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
